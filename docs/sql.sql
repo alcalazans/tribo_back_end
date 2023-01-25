00bfa3568ef331dd229a746c7641eb762ae086c2
Alexandre Calazans Pereira do Amaral <alexandre.calazans@serpro.gov.br>
2023-01-25 15:12:01 -0300

    config: retormando projeto

--- docs/sql.sql
+++ docs/sql.sql
@@ -1,8 +1,12 @@
 --//create schema tribo;
 --//ALTER SCHEMA "Tribo" RENAME TO tribo;
 --
---//CREATE EXTENSION pgcrypto;
---//SELECT encode(digest('@kakashi01', 'sha1'), 'hex');
+--CREATE EXTENSION pgcrypto;
+--SELECT encode(digest('@kakashi01', 'sha1'), 'hex');
+
+select * from usuario u
+select * from usurio
+
 
 DROP TABLE tribo.agendamento;
 DROP TABLE tribo.local;
@@ -214,6 +218,8 @@ INSERT INTO tribo.perfil
 (nome, descricao, situacao, data_criacao)
 VALUES('USUARIO_ADMIN', 'Usuário Administrador', 0, '01/01/2021 10:00');
 
+--select * from tribo.papel
+
 INSERT INTO tribo.perfil_papel
 (id_perfil, id_papel)
 VALUES(1, 1);
@@ -230,43 +236,51 @@ INSERT INTO tribo.perfil_papel
 (id_perfil, id_papel)
 VALUES(2, 2);
 
-INSERT INTO tribo.usuario
+select * from tribo.usuario u
+
+    INSERT INTO tribo.usuario
 (nome, email, cpf, senha, situacao, data_criacao, data_cancelamento)
 VALUES('Alexandre Calazans Pereira do Amaral', 'alexandre.calazans@gmail.com', '83847677187', '453d161e8fb2b1a7118a2fc4811b70f8d57f5c9d', 0, '09/02/2021 10:00', null);
 
 INSERT INTO tribo.usuario
 (nome, email, cpf, senha, situacao, data_criacao, data_cancelamento)
-VALUES('Jonh Lenon', 'johnl@gmail.com', '03029899878', '453d1623232323232fc4811b70f8d57f5c9d', 0, '09/02/2021 10:00', null);
+VALUES('Jonh Lenon', 'johnl@gmail.com', '03029899878', '453d161e8fb2b1a7118a2fc4811b70f8d57f5c9d', 0, '09/02/2021 10:00', null);
 
-INSERT INTO tribo.usuario_perfil
+select * from tribo.usuario_perfil up where id_usuario = 1
+delete from tribo.usuario_perfil up2 where id_usuario = 1 and id_perfil = 2
+
+    INSERT INTO tribo.usuario_perfil
 (id_usuario, id_perfil)
 VALUES(1, 1);
+INSERT INTO tribo.usuario_perfil
+(id_usuario, id_perfil)
+VALUES(1, 2);
 
 INSERT INTO tribo.usuario_perfil
 (id_usuario, id_perfil)
 VALUES(2, 1);
 
-select * from tribo.usuario_perfil
+--select * from tribo.usuario_perfil
+--
+--select * from tribo.perfil
+--select * from tribo.usuario
 
-select * from tribo.perfil
-select * from tribo.usuario
 
-
-    INSERT INTO tribo.endereco_usuario
+INSERT INTO tribo.endereco_usuario
 (id_usuario, logradouro, endereco_numero, complemento, bairro, cep, nome_municipio, pais, uf)
 VALUES(1, 'Área Especial', '4', 'I J Sports Club torre 2 apartamento 1802', 'Guará', '71070640', 'Brasília', 0, 'DF');
 
 INSERT INTO tribo.area_profissional
 (nome, descricao, situacao, data_criacao)
-VALUES('Marcenaria', 'Marcenaria', 0, '31/12/2020 00:00');
+VALUES('Marcenaria', 'Marcenaria', 0, '12/31/2020 00:00');
 
 INSERT INTO tribo.servico
 (nome, descricao, situacao, data_criacao, id_usuario, id_area_profissional, area_profissional_sugestao, abrangencia)
-VALUES('Marcenaria', 'Móveis, reforma de móveis', 0, '02/01/2021 10:00', 2, 1, '', 0);
+VALUES('Marcenaria', 'Móveis, reforma de móveis', 0, '01/02/2021 10:00', 2, 1, '', 0);
 
-select u.nome,u.email, s.nome, s.descricao , ap.descricao from tribo.servico s, tribo.area_profissional ap, tribo.usuario u
-where ap.id = s.id_area_profissional and s.id_usuario = u.id and ap.id = 1;
-
+--select u.nome,u.email, s.nome, s.descricao , ap.descricao from tribo.servico s, tribo.area_profissional ap, tribo.usuario u
+--where ap.id = s.id_area_profissional and s.id_usuario = u.id and ap.id = 1;
+----- SEGUNDA PARTE
 INSERT INTO tribo.contrato
 (id_servico, id_usuario_contratante, id_usuario_prestador, situacao, data_criacao, data_conclusao)
 VALUES(1, 1, 2, 0, '02/02/2021 10:20', '02/02/2021 10:20');
@@ -279,8 +293,8 @@ INSERT INTO tribo.mensagem
 (id_contrato, mensagem, id_usuario_remetente, id_usuario_receptor, situacao, data_envio, data_leitura)
 VALUES(1, 'Gostaria de realizar um orçamento', 1, 2, 0, '02/02/2021 10:20', '02/02/2021 10:20');
 
-select c.id as contrato, m.mensagem as mensagem, hc.descricao as historico from tribo.contrato c , tribo.mensagem m , tribo.historico_contrato hc
-where c.id = m.id_contrato and c.id = hc.id_contrato and c.id = 1;
+--select c.id as contrato, m.mensagem as mensagem, hc.descricao as historico from tribo.contrato c , tribo.mensagem m , tribo.historico_contrato hc
+--where c.id = m.id_contrato and c.id = hc.id_contrato and c.id = 1;
 
 
 INSERT INTO tribo.historico_contrato
@@ -291,17 +305,17 @@ INSERT INTO tribo.mensagem
 (id_contrato, mensagem, id_usuario_remetente, id_usuario_receptor, situacao, data_envio, data_leitura)
 VALUES(1, 'Vamos sim realizar o orçamento', 2, 1, 0, '03/02/2021 10:20', '03/02/2021 10:20');
 
-select m.mensagem as mensagem, m.data_envio from tribo.contrato c , tribo.mensagem m
-where c.id = m.id_contrato and c.id = 1 order by m.data_envio;
-
-select hc.acao, hc.descricao, hc.data_criacao from tribo.contrato c , tribo.historico_contrato hc
-where c.id = hc.id_contrato and c.id = 1 order by hc.data_criacao;
-
-
-select * from tribo.usuario;
-select * from tribo.perfil;
-
-select * from tribo.usuario_perfil;
+--select m.mensagem as mensagem, m.data_envio from tribo.contrato c , tribo.mensagem m
+--where c.id = m.id_contrato and c.id = 1 order by m.data_envio;
+--
+--select hc.acao, hc.descricao, hc.data_criacao from tribo.contrato c , tribo.historico_contrato hc
+--where c.id = hc.id_contrato and c.id = 1 order by hc.data_criacao;
+--
+--
+--select * from tribo.usuario;
+--select * from tribo.perfil;
+--
+--select * from tribo.usuario_perfil;
 
 
 select
